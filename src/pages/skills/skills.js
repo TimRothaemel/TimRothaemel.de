d97014738bb2c1cf65b4e0dad99b3a3d0cbd6c19
Tim Rothaemel <timrothamel@gmail.com>
2026-02-23 23:04:36 +0100

    under 800px the calendar isn't shown

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -1,3 +1,6 @@
 document.addEventListener("headerLoaded", function () {
   document.getElementById("skills-btn")?.classList.add("active-btn"); // Highlight the current page in the header
 });
+GitHubCalendar(".calendar", "TimRothaemel", { //add calendar to page
+  responsive: true,
+});
