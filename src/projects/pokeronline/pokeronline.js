17b4683b50fd79d57265726210705e627e003543
Tim Rothaemel <timrothamel@gmail.com>
2026-04-28 22:30:38 +0200

    docs: add pokeronline to the projects

---
+++ src/projects/pokeronline/pokeronline.js
@@ -0,0 +1,18 @@
+document.addEventListener("headerLoaded", function () {
+  document.getElementById("project-btn")?.classList.add("active-btn"); // Highlight the current page in the header
+});
+
+async function loadReadme() {
+  try {
+    const res = await fetch('https://github.com/TimRothaemel/pokeronline/blob/main/README.md');
+    const md = await res.text();
+    document.getElementById('readme-content').innerHTML = marked.parse(md);
+  } catch (error) {
+    console.error('Error loading README:', error);
+    document.getElementById('readme-content').innerHTML = '<p>README konnte nicht geladen werden.</p>';
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+loadReadme();
+});
